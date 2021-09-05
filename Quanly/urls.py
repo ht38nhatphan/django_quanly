72bd20960122f9800b42993737991197f276c6aa
Huy Hoang <hoangvanhuy1001@gmail.com>
2021-09-05 20:18:41 +0700

    testloi

--- Quanly/urls.py
+++ Quanly/urls.py
@@ -10,13 +10,9 @@ urlpatterns = [
     path('qlnv/', views.staff, name='staff'), 
     path('qlkh/', views.customer, name='customer'), 
     path('addkh/', views.addcustomer, name='addcustomer'), 
-<<<<<<< HEAD
-    # path('addod/', views.addOrder, name='addorder'),
     path('od/', views.order, name='order'),
-=======
     path('qlkh/<int:id>/', views.editcustomer, name='edit_customer'), 
     path('qlkh/de/<int:id>/', views.deletecustomer, name='delete_customer'), 
->>>>>>> 8b64d173b7625fceea3f845cacd036049f6bb513
     # path('savekh/', views.saveCustomer, name='savecustomer'),
    
 ]
