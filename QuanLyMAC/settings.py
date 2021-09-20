f2e7c4de9964de7622c9dd34dd6ccca511094fd6
ht38nhatphan <teonhat101@gmail.com>
2021-09-20 10:45:59 +0700

    set up vession new

--- QuanLyMAC/settings.py
+++ QuanLyMAC/settings.py
@@ -80,14 +80,18 @@ WSGI_APPLICATION = 'QuanLyMAC.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Quanlytramtron',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'USER': 'root',
-        'PASSWORD': '0129nhat',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'Quanlytramtron',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '3306',
+    #     'USER': 'root',
+    #     'PASSWORD': '0129nhat',
         
+    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
